bfbb3af9f0704771d6c7812a5dff53a9c747f322
Favour okerri <favourokerri767@gmail.com>
2024-01-31 04:32:28 -1100

    lint

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -4,7 +4,7 @@ process.stdin.setEncoding('utf8');
 
 process.stdin.on('readable', () => {
   const data = process.stdin.read();
-  if (data != null) { 
+  if (data != null) {
     process.stdout.write(`Your name is: ${data.toString()}`);
     process.exit();
   }
