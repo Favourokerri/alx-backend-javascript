e49231206f6f732562d47f701b3bcef1d45aa594
Favour okerri <favourokerri767@gmail.com>
2024-01-31 04:09:53 -1100

    .\1-stdin.js

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,16 @@
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+
+process.stdin.setEncoding('utf8');
+
+process.stdin.on('readable', () => {
+  const data = process.stdin.read();
+  if (data != null) { 
+    process.stdout.write(`Yourname is: ${data.toString().trim()}\n`);
+    process.exit();
+  }
+});
+
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
+  process.exit();
+});
